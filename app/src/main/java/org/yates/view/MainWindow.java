35e100a288271796e8ba72a0b78f4bc9415d7562
Jordan Yates <yates.j.a@outlook.com>
2023-12-03 20:01:43 -0500

    Implement FileActions interface
    Temporary stopgap until a more defined controller is made, as MainWindow should be focused on being a View

--- app/src/main/java/org/yates/view/MainWindow.java
+++ app/src/main/java/org/yates/view/MainWindow.java
@@ -1,30 +1,45 @@
-package org.yates.ui;
+package org.yates.view;
 
+import org.yates.controller.FileActions;
 import javax.swing.JFrame;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import net.miginfocom.swing.MigLayout;
-
-public class MainWindow extends JFrame {
+public class MainWindow extends JFrame implements FileActions {
 	private static final int DEFAULT_WIDTH   = 525;
 	private static final int DEFAULT_HEIGHT  = 600;
 	private static final String WINDOW_TITLE = "Tabularium";
 
-	private static final Logger logger = LoggerFactory.getLogger(MainWindow.class);
+	private static final Logger log = LoggerFactory.getLogger(MainWindow.class);
 
 	public void start() {
-		logger.info("Starting");
+		log.info("Starting");
 		TabulariumLaf.setLookAndFeel();
 		setTitle(WINDOW_TITLE);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 		setLocationRelativeTo(null);
+		setJMenuBar(new DatabaseMenuBar(this));
 
 		WelcomeSplash splash = new WelcomeSplash();
 		add(splash);
 
 		setVisible(true);
+	}
+
+	@Override
+	public void openFile() {
+		log.info("Opening file...");		
+	}
+
+	@Override
+	public void newFile() {
+		log.info("Creating new file...");
+	}
+
+	@Override
+	public void saveFile() {
+		log.info("Saving file...");
 	}
 }
